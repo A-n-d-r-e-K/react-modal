eb0eaef390647997b0f96b3cf739fa469caf7a65
Andrei K <275991@gmail.com>
2022-10-27 01:03:48 +0300

    img edit

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ function App() {
       {open && (
         <div className="overlay">
           <div className="modal">
-            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+            <svg
+              onClick={() => setOpen(false)}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
               <title />
               <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
             </svg>
-            
+
             <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
 
             {/* https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif */}
@@ -24,7 +29,6 @@ function App() {
             {/* <img src="https://media2.giphy.com/media/m6OomwWCojfS8/giphy.gif" /> */}
 
             {/* <div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/CJyRX8btFziYLt3WAT/video" width="480"></iframe></div> */}
-          
           </div>
         </div>
       )}
